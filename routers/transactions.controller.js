80e43d746ec18e9679f19e0c176dc5cc6767f3c2
ihahalev <i_hahalev@ukr.net>
2020-10-29 10:13:29 +0200

    statsAnual init

--- routers/transactions.controller.js
+++ routers/transactions.controller.js
@@ -19,19 +19,6 @@ class TransactionController {
 
   async createTransaction(req, res) {
     try {
-      const user = await userModel.findById(req.user);
-      if (!user.familyId) {
-        throw new ApiError(403, 'Forbidden', {
-          message: 'Not part of a Family',
-        });
-      }
-      const family = familyModel.findById(user.familyId);
-      if (!family) {
-        throw new ApiError(403, 'Forbidden', {
-          message: 'Not part of a Family',
-        });
-      }
-
       const { amount, type, category, comment } = req.body;
       const {
         _id,
@@ -68,6 +55,42 @@ class TransactionController {
     }
   }
 
+  async getAnnualStats(req, res) {
+    try {
+      const { familyId } = req.user;
+      const { month, year } = req.params;
+      const transes = await transactionModel.getFamilyAnnualReport(
+        familyId,
+        month,
+        year,
+      );
+      return responseNormalizer(200, res, { transes });
+    } catch (e) {
+      errorHandler(req, res, e);
+    }
+  }
+
+  async familyAuthorization(req, res, next) {
+    try {
+      const user = await userModel.findById(req.user);
+      if (!user.familyId) {
+        throw new ApiError(403, 'Forbidden', {
+          message: 'Not part of a Family',
+        });
+      }
+      const family = familyModel.findById(user.familyId);
+      if (!family) {
+        throw new ApiError(403, 'Forbidden', {
+          message: 'Not part of a Family',
+        });
+      }
+      req.user = user;
+      next();
+    } catch (e) {
+      errorHandler(req, res, e);
+    }
+  }
+
   validateTransactionObject(req, res, next) {
     try {
       const { error: validationError } = Joi.object({
@@ -76,6 +99,23 @@ class TransactionController {
         category: Joi.string().min(3),
         comment: Joi.string().min(3),
       }).validate(req.body);
+
+      if (validationError) {
+        throw new ApiError(400, 'Bad requiest', validationError);
+      }
+
+      next();
+    } catch (e) {
+      errorHandler(req, res, e);
+    }
+  }
+
+  validateAnnualStatsParams(req, res, next) {
+    try {
+      const { error: validationError } = Joi.object({
+        year: Joi.number().positive().integer().min(1970).required(),
+        month: Joi.number().positive().integer().min(0).max(11).required(),
+      }).validate(req.params);
 
       if (validationError) {
         throw new ApiError(400, 'Bad requiest', validationError);
--- routers/transactions.router.js
+++ routers/transactions.router.js
@@ -3,8 +3,22 @@ const router = express.Router();
 const transactionsController = require('./transactions.controller');
 const authCheck = require('../middlewares/auth');
 
-router.post('/', authCheck, transactionsController.validateTransactionObject, transactionsController.createTransaction);
+router.post(
+  '/',
+  authCheck,
+  transactionsController.familyAuthorization,
+  transactionsController.validateTransactionObject,
+  transactionsController.createTransaction,
+);
 
 router.get('/categories', authCheck, transactionsController.getCategories);
+
+router.get(
+  '/stats/annual',
+  authCheck,
+  transactionsController.familyAuthorization,
+  transactionsController.validateAnnualStatsParams,
+  transactionsController.getAnnualStats,
+);
 
 module.exports = router;
