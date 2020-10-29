7c819f8c8fd5a5fdd11cd9e6efbb346745c60bb7
nNg9999 <n_n_g_@ukr.net>
2020-10-30 01:45:33 +0200

    fixed rewiev

--- cron/getIncrementBalance.js
+++ cron/getIncrementBalance.js
@@ -1,5 +1,5 @@
 const cron = require('node-cron');
-const { familyModel, userModel, transactionModel } = require('../database/models');
+const { familyModel } = require('../database/models');
 
 const { getLogger } = require('../helpers');
 const logger = getLogger('getIncrementBalance');
@@ -11,27 +11,11 @@ async function main() {
       const allFamilies = await familyModel.find({});
 
       await Promise.all(allFamilies.map(async item => {
-        await item.update(
-          {
-            $inc: {
-              giftsForUnpacking: Math.floor((item.balance * item.flatSquareMeters / item.flatPrice) - item.giftsUnpacked)
-            }
-          }
-        )
 
-        const user = await userModel.findOne({ familyId: item.familyId });
-        const sum = item.totalSalary + item.passiveIncome;
+        item.balance += item.totalSalary + item.passiveIncome;
+        item.giftsForUnpacking = Math.floor((item.balance * item.flatSquareMeters / item.flatPrice) - item.giftsUnpacked)
+        await item.save();
 
-        // awaiting transactionModal realization 
-        const itemBalance = await transactionModel.mounthlyAccrual(sum, item.incomePercentageToSavings, user._id, item._id);
-
-        item.update(
-          {
-            $inc: {
-              balance: itemBalance,
-            }
-          }
-        )
       }));
 
       logger.info(`FamilyModel increment balance 1-st day of month (00:00)`);
