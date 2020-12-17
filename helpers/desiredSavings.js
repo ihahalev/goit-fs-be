82c5c496a66e8f86e98aca9188add1c0cd699f11
ihahalev <i_hahalev@ukr.net>
2020-12-17 22:58:05 +0200

    savings calc refactor

--- helpers/desiredSavings.js
+++ helpers/desiredSavings.js
@@ -1,6 +1,6 @@
 module.exports = function () {
   const sum = parseFloat(this.totalSalary) + parseFloat(this.passiveIncome);
-  const savings = (sum * parseFloat(this.incomePercentageToSavings)) / 100;
+  const savings = (sum * parseFloat(this.incomePercentageToSavings)) / 100.0;
   console.log('savings', savings, sum);
   return savings.toFixed(2);
 };
